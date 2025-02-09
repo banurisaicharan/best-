7613ebf65ea02c305812a37d3a0e70acf20142e1
banurisaicharan <banurisaicharan@gmail.com>
2025-02-09 14:47:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-resource "azurerm_resource_group" "mafia" {
-  name = "${var.rg}-${var.client}-${var.env}-${var.prefix}"
-  location = var.location
+resource "aws_vpc" "mafia2" {
+  cidr_block = ["10.0.0.2/16"]
 }
